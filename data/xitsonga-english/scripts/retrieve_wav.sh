6fce05bedf455509675c99d8b68f3cc3115c3bdf
mde <maureen.deseyssel@gmail.com>
2019-11-20 17:43:56 +0100

    updated data-prep xitsonga-english scripts

--- data/xitsonga-english/scripts/retrieve_wav.sh
+++ data/xitsonga-english/scripts/retrieve_wav.sh
@@ -4,6 +4,7 @@ dataset=$1
 files_list=$2
 tgt_dir=$3 # directory which will contain symlinks of the wav files. 
 
+#TODO add exception error and check handling
 
 if [ "$#" -ne 3 ]; then
     echo "You must enter exactly 3 command line arguments"
@@ -12,10 +13,29 @@ if [ "$#" -ne 3 ]; then
 fi
 
 
-if [ "$dataset" == "timit" ]; then
+if [ "$dataset" == "nchlt-xitsonga" ]; then
+
+
+    path_ds=/scratch1/data/raw_data/NCHLT/nchlt_Xitsonga/audio
+
+    mkdir -p ${tgt_dir}
+    
+    cat $files_list | while read l; do
+        a=$(echo $l | cut -d'_' -f3)
+        b=$(echo $l | cut -d'_' -f4)
+        a_prime=$(echo $a | sed 's/[a-z]//g') # a bit farfetched. need to make it cleaner. 
+
+        ln -s "${path_ds}/${a_prime}/nchlt_tso_${a}_${b}.wav" ${tgt_dir}/${l}
+        echo "Symlinked file ${l} to ${tgt_dir}."
+
+
+        
+    done
+
+
+elif [ "$dataset" == "timit" ]; then
 
     path_ds=/scratch1/data/raw_data/TIMIT
-
     mkdir -p ${tgt_dir}
     
     cat $files_list | while read l; do
@@ -23,19 +43,15 @@ if [ "$dataset" == "timit" ]; then
         b=$(echo $l | cut -d'_' -f2)
 
         cur_dir=$(find $path_ds -type d -name $a)
-        # path_to_file=$(find $cur_dir -type f -name ${b}.wav | readlink -f {} \;)
         find $cur_dir -type f -name ${b}.wav -exec ln -s {} ${tgt_dir}/${l} \;
         echo "Symlinked file ${l} to ${tgt_dir}."
 
     done
 
 
-elif [ "$dataset" == "xitsonga" ]; then
-
-echo "not implemented yet"
-
+    
 else
 
-    echo 'Please enter the dataset you want to take care of. Can be "timit" or "xitsonga". --> exiting' && exit 1
+    echo 'Please enter the dataset you want to take care of. Can be "timit" or "nchlt-xitsonga". --> exiting' && exit 1
 
 fi
