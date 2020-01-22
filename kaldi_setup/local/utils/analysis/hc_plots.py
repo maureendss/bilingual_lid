55dd9eec63fc1e027619127b19328e45a424b9f7
mde <maureen.deseyssel@gmail.com>
2020-01-22 18:12:50 +0100

    added scripts for hierarchical clustering

---
+++ kaldi_setup/local/utils/analysis/hc_plots.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+# from local.utils.analysis.hierarchical_clustering import *
+from hierarchical_clustering import *
+ 
+
+def run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=None):
+    purity_list=[]
+    for ivec_scp, datadir in zip(ivecs, data_dirs):                                                                                                                                 
+        ivectors_df, labels_df = prepare_data(ivec_scp, datadir)                                                                                                                    
+        purity_list.append(get_purity_range(prange, np.array(ivectors_df), labels_df[label]))                                                                                      
+        plot_purity(purity_list, out_fig, ivecs_names, label, style_list=style_list)                                                                                                
+    for p, name in zip(purity_list, ivecs_names):                                                                                                                               
+        c = len(set(labels_df[label]))
+        print("--------")
+        print("Purity for {} clusters on {} label for {} is {}".format(c, label, name, dict((x, y) for x, y in p)[c]))     
+
+
+
+
+
+
+if __name__ == '__main__' :
+    
+    ######### TRAIN #############
+    
+
+    # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Create Train plots per lang
+    ivecs_names=['mix_spkmatch_eng-ger', 'mix_spkmatch_eng-fin', 'bil_eng-ger', 'bil_eng-fin']
+    ivecs=['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_{}_ts-train_{}/ivector.scp'.format(n, n) for n in ivecs_names]                                                                       
+    data_dirs=['data/emime/train_{}'.format(n) for n in ivecs_names]     
+    style_list=[('r','-.'), ('b', '-.'), ('r',':'), ('b', ':')]
+    label='lang'
+    out_fig='figs/hc/emime-deltassdc/hc_train_lang.pdf'
+    prange=(2,20)
+    
+    run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=style_list)
+    
+    
+    # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #### Create Train plots per spk. USE spkmatch this time as need all same number of spks.
+    
+    ivecs_names=['mono_eng_native', 'mono_eng', 'mono_ger', 'mono_fin', 'mix_spkmatch_eng-ger', 'mix_spkmatch_eng-fin', 'bil_eng-ger', 'bil_eng-fin']
+    ivecs=['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_{}_ts-train_{}/ivector.scp'.format(n, n) for n in ivecs_names]
+    data_dirs=['data/emime/train_{}'.format(n) for n in ivecs_names]
+    style_list=[('mediumseagreen', '--'), ('forestgreen', '--'), ('r', '--'), ('b', '--'), ('r','-.'), ('b', '-.'), ('r',':'), ('b', ':')]
+    label='spk'
+    out_fig='figs/hc/emime-deltassdc/hc_train_spk.pdf'
+    prange=(12,30)
+    
+    run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=style_list)
+    
+    
+    # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #### Create Train plots per SENT
+    
+    ivecs_names=['mix_spkmatch_eng-ger', 'mix_spkmatch_eng-fin', 'bil_eng-ger', 'bil_eng-fin']
+    ivecs=['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_{}_ts-train_{}/ivector.scp'.format(n, n) for n in ivecs_names]                                                                       
+    data_dirs=['data/emime/train_{}'.format(n) for n in ivecs_names]     
+    style_list=[('r','-.'), ('b', '-.'), ('r',':'), ('b', ':')]
+    label='sent'
+    out_fig='figs/hc/emime-deltassdc/hc_train_sent.pdf'
+    prange=(280,320)
+    
+    run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=style_list)
+    
+    
+    ######### TEST #############
+    
+    #----------------------------
+    #------- LANG ---------------
+    
+    for lang in ['fin', 'ger']:
+        for t_type in ['bil', 'mixed']:
+            
+            # -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+            
+            ivecs_names=['mono_eng_native', 'mono_eng','mono_{}'.format(lang),'mix_spkmatch_eng-{}'.format(lang), 'bil_eng-{}'.format(lang)]
+            ivecs_test_name='test_eng-{}-{}'.format(lang,t_type)
+            ivecs=['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_{}_ts-{}/ivector.scp'.format(n, ivecs_test_name) for n in ivecs_names]                                                                       
+            data_dirs=['data/emime/{}'.format(ivecs_test_name)]*len(ivecs_names)     
+            style_list=[('mediumseagreen','--'), ('forestgreen', '--'), ('lime', '--'),('r', '-.'), ('r',':')]
+            label='lang'
+            out_fig='figs/hc/emime-deltassdc/hc_test_{}_lang_eng-{}.pdf'.format(t_type, lang)
+            prange=(2,20)
+            
+            run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=style_list)
+            
+            
+    #----------------------------
+    #------- SPK ---------------
+    
+    for lang in ['fin', 'ger']:
+        for t_type in ['bil', 'mixed']:
+            
+            # -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+            #### Create Test plots per lang - ENG-GER
+            ivecs_names=['mono_eng_native', 'mono_eng','mono_{}'.format(lang),'mix_spkmatch_eng-{}'.format(lang), 'bil_eng-{}'.format(lang)]
+            ivecs_test_name='test_eng-{}-{}'.format(lang,t_type)
+            ivecs=['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_{}_ts-{}/ivector.scp'.format(n, ivecs_test_name) for n in ivecs_names]                                                                       
+            data_dirs=['data/emime/{}'.format(ivecs_test_name)]*len(ivecs_names)     
+            style_list=[('mediumseagreen','--'), ('forestgreen', '--'), ('lime', '--'),('r', '-.'), ('r',':')]
+            label='spk'
+            out_fig='figs/hc/emime-deltassdc/hc_test_{}_spk_eng-{}.pdf'.format(t_type, lang)
+            if t_type == 'bil':
+                prange=(2,20)
+            else:
+                prange=(4,20)
+                
+                run_hc(ivecs_names, ivecs, data_dirs, label, prange, out_fig, style_list=style_list)
--- kaldi_setup/local/utils/analysis/hierarchical_clustering.py
+++ kaldi_setup/local/utils/analysis/hierarchical_clustering.py
@@ -9,127 +9,125 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.cluster import homogeneity_score
 import scipy.cluster.hierarchy as shc
+from matplotlib.ticker import MaxNLocator
 
 #just notes on log reg. Not to be run as is.
 
 #train='train_bil_eng-ger'
 
-if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("ivec_scp", help="")
-    parser.add_argument("data_dir", help="")
-#    parser.add_argument("output_dendrogram", help="")
-    parser.add_argument('--plot', dest='plot', action='store_true')
-    parser.parse_args()
-    args, leftovers = parser.parse_known_args()
-
-
-    with kaldiio.ReadHelper('scp:'+args.ivec_scp) as reader:
+def prepare_data(ivec_scp, data_dir):
+    
+    with kaldiio.ReadHelper('scp:'+ivec_scp) as reader:
         ivectors={}
         for k, iv in reader:
             ivectors[k]=iv
 
 
-    with open('{}/utt2lang'.format(args.data_dir), 'r') as input_utt2lang:
+    with open('{}/utt2lang'.format(data_dir), 'r') as input_utt2lang:
         utt2lang_dict={}
         for line in input_utt2lang:
             utt2lang_dict[line.split(' ')[0]] = line.split(' ')[1].strip('\n')
 
-    with open('{}/utt2spk'.format(args.data_dir), 'r') as input_utt2spk:
+    with open('{}/utt2spk'.format(data_dir), 'r') as input_utt2spk:
         utt2spk_dict={}
         for line in input_utt2spk:
             utt2spk_dict[line.split(' ')[0]] = line.split(' ')[1].strip('\n')
 
-    with open('{}/utt2sent'.format(args.data_dir), 'r') as input_utt2sent:
+    with open('{}/utt2sent'.format(data_dir), 'r') as input_utt2sent:
         utt2sent_dict={}
         for line in input_utt2sent:
             utt2sent_dict[line.split(' ')[0]] = line.split(' ')[1].strip('\n')
-
             
     ivectors_df = pd.DataFrame.from_dict(ivectors, orient='index').sort_index()
-
     labels_df = pd.DataFrame.from_dict(utt2lang_dict, orient='index', columns=["lang"]).sort_index()
     labels_df["spk"]=pd.DataFrame.from_dict(utt2spk_dict, orient='index')
     labels_df["sent"]=pd.DataFrame.from_dict(utt2sent_dict, orient='index')
 
-    #data = np.array(list(ivectors.values().sort()))
     data=np.array(ivectors_df)
-    #ivectors_df = pd.DataFrame.from_dict(ivectors, orient='index')
-    cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+
+    return ivectors_df, labels_df
+
+
+
+
+def get_purity(c, data, label_data, linkage_proc='ward'):
+    cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage=linkage_proc)
     data_p=cluster.fit_predict(data)
-
-
-    #max number of clusters for perfect purity score
-
-    homog_lang=[]
-    for c in range(len(set(labels_df["lang"]))+1, 21):
-        cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage='ward')
-        data_p=cluster.fit_predict(data)
-        homog_score = homogeneity_score(data_p, labels_df["lang"])
-        homog_lang.append((c, homog_score))
-
-    homog_spk=[]
-    for c in range(len(set(labels_df["spk"]))+1, 21):
-        cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage='ward')
-        data_p=cluster.fit_predict(data)
-        homog_score = homogeneity_score(data_p, labels_df["spk"])
-        homog_spk.append((c, homog_score))
-
-
-    # print("Purity is reached for lang with {} clusters".format(c))
-
+    return homogeneity_score(label_data, data_p)
     
-    # c=len(set(labels_df["lang"])) -1 
-    # homog_score=0.0
-    # while homog_score < 1.0:
-    #     if c > len(data):
-    #         print("Clusters never reaching lang purity for {}".format(args.ivec_scp))
-    #     c += 1
-    #     cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage='ward')
-    #     data_p=cluster.fit_predict(data)
-    #     homog_score = homogeneity_score(data_p, labels_df["lang"])
-    #     print("Homog of {} with {} clusters".format(homog_score, c))
 
-    # print("Purity is reached for lang with {} clusters".format(c))
+def get_purity_range(c_range, data, label_data, linkage_proc='ward'):
+    #c_range is a tuple of form (2,20) if want scores for 2 to 21
+    purity_range=[]
+    for x in range(c_range[0], c_range[1]+1):
+        purity_range.append((x, get_purity(x, data, label_data, linkage_proc=linkage_proc)))
+    return purity_range
 
 
-    # c=len(set(labels_df["spk"])) -1
-    # homog_score=0.0
-    # while homog_score < 1.0:
-    #     if c > len(data):
-    #         print("Clusters never reaching speaker purity for {}".format(args.ivec_scp))
-    #     c += 1
-    #     cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage='ward')
-    #     data_p=cluster.fit_predict(data)
-    #     homog_score = homogeneity_score(data_p, labels_df["spk"])
-    #     print("Homog of {} with {} clusters".format(homog_score, c))
-
-    # print("Purity is reached for spk with {} clusters".format(c))
-
-
-    # c=len(set(labels_df["sent"])) -1
-    # homog_score=0.0
-    # while homog_score < 1.0:
-    #     if c > len(data):
-    #         print("Clusters never reaching sentence purity for {}".format(args.ivec_scp))
-    #     c += 1 
-    #     cluster = AgglomerativeClustering(n_clusters=c, affinity='euclidean', linkage='ward')
-    #     data_p=cluster.fit_predict(data)
-    #     homog_score = homogeneity_score(data_p, labels_df["sent"])
-    #     print("Homog of {} with {} clusters".format(homog_score, c))
-        
-    # print("Purity is reached for sent with {} clusters".format(c))
-
-
-
-    if args.plot:
-        #dendogram plot
-        plt.figure(figsize=(10, 7))
-        plt.title("Dendograms for {}".format(args.ivec_scp.split('/')[2]))
-        dend = shc.dendrogram(shc.linkage(data, method='ward'))
-        plt.savefig(args.output_dendrogram)
-
-
+def plot_purity(purity_list, out_fig, ivec_names,label_name, style_list=None ):
+    #ivec_names i s a list of length of purity_list, with the name of each ivector for the legend
+    #purity is of form [[(c_a1,purity_a1)(c_a2, purity_a2)],[(c_b1,purity_b1)(c_b2, purity_b2)]]
+    #style_list is a list of same size as the purity list with tuple of color and linestyle. eg: [('r','--'), ('b', '-')]
     
+    plt.figure()
+
+    plt.subplot(211)
+    ax = plt.figure().gca()
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    if style_list:
+        for group, label,style in zip(purity_list, ivec_names, style_list):
+            x, y = zip(*group) # unpack a list of pairs into two tuples
+            plt.plot(x, y, label=label, color=style[0], linestyle=style[1])
+    else:
+        for group, label in zip(purity_list, ivec_names):
+            x, y = zip(*group) # unpack a list of pairs into two tuples
+            plt.plot(x, y, label=label)
+
+#    plt.legend()    
+#    plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+    lgd = plt.legend(bbox_to_anchor=(1, 0.5), loc='center left')
+    plt.ylabel('{} purity'.format(label_name))
+    plt.xlabel('Number of clusters')
+    plt.savefig(out_fig,bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.close()
+# if __name__ == "__main__":
+#     import argparse
+
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("ivec_scp", help="")
+#     parser.add_argument("data_dir", help="")
+#     #    parser.add_argument("output_dendrogram", help="")
+#     parser.add_argument('out_fig')
+#     parser.add_argument('--plot', dest='plot', action='store_true')
+
+#     parser.parse_args()
+#     args, leftovers = parser.parse_known_args()
+
+
+
+#     ivectors_df, labels_df = prepare_data(args.ivec_scp, args.data_dir) 
+
+#     get_purity(2, data, labels_df['lang'], linkage='ward')
+
+
+#     data=np.array(ivectors_df)
+#     #ivectors_df = pd.DataFrame.from_dict(ivectors, orient='index')
+
+
+
+
+ivecs = ['exp_emime/ivectors-deltassdc/ivectors_128_tr-train_mix_eng-ger_ts-train_mix_eng-ger/ivector.scp', 'exp_emime/ivectors-deltassdc/ivectors_128_tr-train_mix_eng-fin_ts-train_mix_eng-fin/ivector.scp', 'exp_emime/ivectors-deltassdc/ivectors_128_tr-train_bil_eng-ger_ts-train_bil_eng-ger/ivector.scp', 'exp_emime/ivectors-deltassdc/ivectors_128_tr-train_bil_eng-fin_ts-train_bil_eng-fin/ivector.scp']
+
+data_dirs= ['data/emime/train_mix_eng-ger', 'data/emime/train_mix_eng-fin', 'data/emime/train_bil_eng-ger', 'data/emime/train_bil_eng-fin']
+ivecs_names=['mix_eng-ger', 'mix_eng-fin', 'bil_eng-ger', 'bil_eng-fin']
+style_list=[('r','-.'), ('b', '-.'), ('r',':'), ('b', ':')]
+
+
+purity_list=[]
+for ivec_scp, datadir in zip(ivecs, data_dirs):
+    ivectors_df, labels_df = prepare_data(ivec_scp, datadir)
+    purity_list.append(get_purity_range((2,20), np.array(ivectors_df), labels_df['lang']))
+
+plot_purity(purity_list, 'tmp.svg', ivecs_names, 'lang', style_list=style_list) 
